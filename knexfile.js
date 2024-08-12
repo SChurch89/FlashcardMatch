a485ab51d2d0b506e6e0ae1f16c3ffd7cff6bb4d
Stephanie Church <98600766+SChurch89@users.noreply.github.com>
2024-08-12 07:48:54 -0400

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+console.log('Database host:', process.env.DB_HOST);
+
+module.exports = {
+  client: 'mysql', 
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
+};
